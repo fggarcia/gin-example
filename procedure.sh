bd99c57a0cd4f98a9a0d1fa3a41eac65c46751c8
Federico German Garcia Ronca <fedemv@gmail.com>
2023-05-01 21:23:41 -0300

    sleep 1

--- procedure.sh
+++ procedure.sh
@@ -1,27 +1,27 @@
 echo "PROCESSING FILE: albums.json (120 MB)...."
 curl -X GET http://localhost:8080/albums/file/albums
 
-sleep 5
+sleep 1
 
 echo "CREATING FIRST HEAP...."
 curl -X GET http://localhost:8080/debug/pprof/heap > heap_first.out
 
-sleep 5
+sleep 1
 
 echo "PROCESSING FILE: albums_2.json (60 MB)...."
 curl -X GET http://localhost:8080/albums/file/albums_2
 
-sleep 5
+sleep 1
 
 echo "CREATING SECOND HEAP...."
 curl -X GET http://localhost:8080/debug/pprof/heap > heap_not_alloc.out
 
-sleep 5
+sleep 1
 
 echo "PROCESSING FILE: albums_3.json (180 MB)...."
 curl -X GET http://localhost:8080/albums/file/albums_3
 
-sleep 5
+sleep 1
 
 echo "CREATING THIRD HEAP...."
 curl -X GET http://localhost:8080/debug/pprof/heap > heap_alloc.out
@@ -29,7 +29,7 @@ curl -X GET http://localhost:8080/debug/pprof/heap > heap_alloc.out
 echo "RUNNING GC"
 curl -X GET http://localhost:8080/albums/gc
 
-sleep 5
+sleep 1
 
 echo "CREATING FORTH HEAP...."
 curl -X GET http://localhost:8080/debug/pprof/heap > heap_gc.out
