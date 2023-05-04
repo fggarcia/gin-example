f1d9bd49b3517815e3e0877c5b81eefca7d176e6
Federico German Garcia Ronca <fedemv@gmail.com>
2023-05-04 14:24:47 -0300

    move encoder to go_json

--- procedure_fast_cache.sh
+++ procedure_fast_cache.sh
@@ -11,7 +11,7 @@ sleep 1
 echo "RUNNING GC"
 curl -X GET http://localhost:8080/albums/gc
 
-sleep 1
+sleep 5
 
 echo "CREATING FAST CACHE HEAP POST GC...."
 curl -X GET http://localhost:8080/debug/pprof/heap > heap_fast_cache_gc.out
